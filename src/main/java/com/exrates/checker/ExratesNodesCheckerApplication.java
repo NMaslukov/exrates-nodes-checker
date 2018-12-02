352ad08cbc928c03e73a796dd49b7df548354ad1
NMaslukov <mikita.malykov@upholding.biz>
2018-12-02 17:46:54 +0200

    init commit

---
+++ src/main/java/com/exrates/checker/ExratesNodesCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.exrates.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExratesNodesCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExratesNodesCheckerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/exrates/checker/ExratesNodesCheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exrates.checker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExratesNodesCheckerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
